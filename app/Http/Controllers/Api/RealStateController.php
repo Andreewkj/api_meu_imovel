5107db387b9fbc2f3e7cc08c91192a78a1111d37
andreewkj <andreew.k.januario@outlook.com>
2023-05-30 19:24:20 -0300

    build: insere classe para retorno das mensagens

--- app/Http/Controllers/Api/RealStateController.php
+++ app/Http/Controllers/Api/RealStateController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Api\ApiMessages;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\RealStateRequest;
 use App\Models\RealState;
-use Illuminate\Http\Client\Response;
 use Illuminate\Http\Request;
 
 class RealStateController extends Controller
@@ -23,12 +24,24 @@ class RealStateController extends Controller
         return Response()->json($realState, 200);
     }
 
-    public function store(Request $request)
+    public function show($id)
+    {
+        try {
+            $realState = $this->realState->findOrfail($id);
+
+            return response()->json([
+                'data' => $realState,
+            ], 200);
+        } catch (\Exception $e) {
+            $message = new ApiMessages($e->getMessage());
+            return response()->json([$message->getMessage()], 401);
+        }
+    }
+
+    public function store(RealStateRequest $request)
     {
         $data = $request->all();
-
         try {
-
             $this->realState->create($data);
 
             return response()->json([
@@ -37,11 +50,12 @@ class RealStateController extends Controller
                 ]
             ], 200);
         } catch (\Exception $e) {
-            return response()->json(['error' => $e->getMessage()], 401);
+            $message = new ApiMessages($e->getMessage());
+            return response()->json([$message->getMessage()], 401);
         }
     }
 
-    public function update($id, Request $request)
+    public function update($id, RealStateRequest $request)
     {
         $data = $request->all();
 
@@ -56,7 +70,26 @@ class RealStateController extends Controller
                 ]
             ], 200);
         } catch (\Exception $e) {
-            return response()->json(['error' => $e->getMessage()], 401);
+            $message = new ApiMessages($e->getMessage());
+            return response()->json([$message->getMessage()], 401);
+        }
+    }
+
+    public function destroy($id)
+    {
+        try {
+            $realState = $this->realState->findOrfail($id);
+
+            $realState->delete();
+
+            return response()->json([
+                'data' => [
+                    'msg' => 'Imóvel removido com sucesso!'
+                ]
+            ], 200);
+        } catch (\Exception $e) {
+            $message = new ApiMessages($e->getMessage());
+            return response()->json([$message->getMessage()], 401);
         }
     }
 }
